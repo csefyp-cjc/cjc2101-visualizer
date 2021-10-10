bb30c3a3904b6861a0b59a0428f80b9e7ae75418
chengmark <mocheng26@gmail.com>
2021-10-10 20:09:38 +0800

    Initial Commit

---
+++ visualizer/visualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  visualizerApp.swift
+//  visualizer
+//
+//  Created by Mark Cheng on 10/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct visualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
