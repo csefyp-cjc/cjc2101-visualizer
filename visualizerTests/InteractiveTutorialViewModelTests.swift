179d3989e88852e6b95fc93846f04ecf80f2fa0e
IamMrandrew <andrewliyanlap000824@gmail.com>
2022-03-09 03:28:19 +0800

    test: add arguments for viewmodels on unit tests

--- visualizerTests/InteractiveTutorialViewModelTests.swift
+++ visualizerTests/InteractiveTutorialViewModelTests.swift
@@ -9,77 +9,95 @@ import XCTest
 @testable import visualizer
 
 class InteractiveTutorialViewModelTests: XCTestCase {
-    var interactiveTutorialViewModel: InteractiveTutorialViewModel?
+    var pitchInteractiveTutorialViewModel: InteractiveTutorialViewModel?
+    var timbreInteractiveTutorialViewModel: InteractiveTutorialViewModel?
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        interactiveTutorialViewModel = InteractiveTutorialViewModel()
+        pitchInteractiveTutorialViewModel = InteractiveTutorialViewModel(page: InteractiveTutorial.Page.pitch)
+        timbreInteractiveTutorialViewModel = InteractiveTutorialViewModel(page: InteractiveTutorial.Page.timbre)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        interactiveTutorialViewModel = nil
+        pitchInteractiveTutorialViewModel = nil
+        timbreInteractiveTutorialViewModel = nil
     }
 
     func test_InteractiveTutorialViewModel_currentPage_shouldBeZero() {
         // Given
         
         // When
-        let vm = InteractiveTutorialViewModel()
+        let vm1 = InteractiveTutorialViewModel(page: InteractiveTutorial.Page.pitch)
+        let vm2 = InteractiveTutorialViewModel(page: InteractiveTutorial.Page.timbre)
         
         // Then
-        XCTAssertEqual(vm.currentPage, 0)
+        XCTAssertEqual(vm1.currentPage, 0)
+        XCTAssertEqual(vm2.currentPage, 0)
     }
     
     func test_InteractiveTutorialViewModel_currentPage_shouldBeIncreased() {
         // Given
-        let vm = InteractiveTutorialViewModel()
-        let prev = vm.currentPage
+        guard let vm1 = pitchInteractiveTutorialViewModel else { XCTFail(); return }
+        guard let vm2 = timbreInteractiveTutorialViewModel else { XCTFail(); return }
         
-        // When
-        let status = vm.handleNextInstruction()
+        for vm in [vm1, vm2] {
+            let prev = vm.currentPage
+            
+            // When
+            let status = vm.handleNextInstruction()
+            
+            // Then
+            XCTAssertNotEqual(vm.currentPage, 0)
+            XCTAssertGreaterThan(vm.currentPage, 0)
+            XCTAssertGreaterThan(vm.currentPage, prev)
+            XCTAssertFalse(status)   // Return false to not dismissing tutorial
+        }
         
-        // Then
-        XCTAssertNotEqual(vm.currentPage, 0)
-        XCTAssertGreaterThan(vm.currentPage, 0)
-        XCTAssertGreaterThan(vm.currentPage, prev)
-        XCTAssertFalse(status)   // Return false to not dismissing tutorial
     }
     
     func test_InteractiveTutorialViewModel_currentPage_shouldAllBeIncreased() {
         // Given
-        let vm = InteractiveTutorialViewModel()
-        let prev = vm.currentPage
+        guard let vm1 = pitchInteractiveTutorialViewModel else { XCTFail(); return }
+        guard let vm2 = timbreInteractiveTutorialViewModel else { XCTFail(); return }
         
-        // When
-        var status: Bool = false
-        let instructionCounts = Int.random(in: 1...vm.tutorials.count-1)
-        for _ in 0..<instructionCounts {
-            status = vm.handleNextInstruction()
+        for vm in [vm1, vm2] {
+            let prev = vm.currentPage
+            
+            // When
+            var status: Bool = false
+            let instructionCounts = Int.random(in: 1...vm.tutorials.count-1)
+            for _ in 0..<instructionCounts {
+                status = vm.handleNextInstruction()
+            }
+            
+            // Then
+            XCTAssertNotEqual(vm.currentPage, 0)
+            XCTAssertGreaterThan(vm.currentPage, prev)
+            XCTAssertLessThan(vm.currentPage, vm.tutorials.count)
+            XCTAssertFalse(status)   // Return true to dismiss tutorial
         }
-        
-        // Then
-        XCTAssertNotEqual(vm.currentPage, 0)
-        XCTAssertGreaterThan(vm.currentPage, prev)
-        XCTAssertLessThan(vm.currentPage, vm.tutorials.count)
-        XCTAssertFalse(status)   // Return true to dismiss tutorial
     }
     
     func test_InteractiveTutorialViewModel_currentPage_shouldNotIncreaseWhenExceedTutorialsLength() {
         // Given
-        guard let vm = interactiveTutorialViewModel else { XCTFail(); return }
-        let prev = vm.currentPage
+        guard let vm1 = pitchInteractiveTutorialViewModel else { XCTFail(); return }
+        guard let vm2 = timbreInteractiveTutorialViewModel else { XCTFail(); return }
         
-        // When
-        var status: Bool = false
-        for _ in 0..<vm.tutorials.count {
-            status = vm.handleNextInstruction()
+        for vm in [vm1, vm2] {
+            let prev = vm.currentPage
+            
+            // When
+            var status: Bool = false
+            for _ in 0..<vm.tutorials.count {
+                status = vm.handleNextInstruction()
+            }
+            
+            // Then
+            XCTAssertNotEqual(vm.currentPage, 0)
+            XCTAssertGreaterThan(vm.currentPage, prev)
+            XCTAssertLessThan(vm.currentPage, vm.tutorials.count)
+            XCTAssertTrue(status)   // Return true to dismiss tutorial
         }
-        
-        // Then
-        XCTAssertNotEqual(vm.currentPage, 0)
-        XCTAssertGreaterThan(vm.currentPage, prev)
-        XCTAssertLessThan(vm.currentPage, vm.tutorials.count)
-        XCTAssertTrue(status)   // Return true to dismiss tutorial
     }
 }
